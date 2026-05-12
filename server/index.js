99a7fb5f206ca1d0c55961b0d0abcc3ac94361b9
AmeliaMatthiesen <143598707+AmeliaMatthiesen@users.noreply.github.com>
2026-05-12 20:16:07 +0200

    Set up PostgreSQL integration and database connection

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-
 const express = require("express");
 const cors = require("cors");
+const pool = require("./db");
 
 const app = express();
 
@@ -10,6 +10,14 @@ app.use(express.json());
 const transactionRoutes = require("./routes/transactionRoutes");
 
 app.use("/", transactionRoutes);
+
+pool.query("SELECT NOW()", (err, res) => {
+  if (err) {
+    console.error("Database connection error:", err);
+  } else {
+    console.log("Database connected:", res.rows);
+  }
+});
 
 app.listen(5000, () => {
   console.log("Server running on port 5000");
